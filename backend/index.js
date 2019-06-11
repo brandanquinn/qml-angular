f868d4c959dcb53accac04e727bba160e138a89a
bquinn <bquinn@silasg.com>
2019-06-11 14:44:51 -0400

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,158 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+
+const get = require('lodash/get');
+
+const {convert_prediction} = require('./utils/convert-predictions');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const mock_pred = {
+    predictions: [
+        {
+            t1: 'TOR',
+            t2: 'GSW',
+            predictedOutcome: 'W',
+            predictedPointdiff: '5'
+        },
+        {
+            t1: 'MIL',
+            t2: 'PHI',
+            predictedOutcome: 'L',
+            predictedPointdiff: '-2'
+        }
+    ]
+};
+
+/**
+ * '/todays-games'
+ * 
+ * NAME
+ *  '/todays-games'
+ * 
+ * DESCRIPTION
+ *  Sets up '/todays-games' route for web app. On loading of page, sends a GET request to python ML model.
+ *  The model then returns list of predictions at today's date as JSON via response.
+ *  The JSON responses are simply printed to web page without formatting for now.
+ * 
+ * AUTHOR
+ *  Brandan Quinn
+ * 
+ * DATE
+ *  5:47pm 2/6/19
+ */
+app.get('/api/todays-games', (req, res) => {
+    // request
+    // .get('http://127.0.0.1:5000/predict')
+    // .on('data', (chunk) => {
+    //     const predictions_json = chunk.toString()
+    //     // Need to pre-process json
+    //     const converted_predictions = get(JSON.parse(predictions_json), 'predictions').map(pred => convert_prediction(pred));
+    //     res.send(converted_predictions);
+    // })
+    // console.log(mock_pred);
+    const converted_predictions = get(mock_pred, 'predictions').map(pred => convert_prediction(pred));
+    res.send(converted_predictions);
+
+    
+});
+
+// var jsonParser = bodyParser.json()
+
+/**
+ * '/predict-by-date'
+ * 
+ * NAME
+ *  '/predict-by-date'
+ * 
+ * DESCRIPTION
+ *  Sets up '/predict-by-date' route for web app to send a POST request with a date string to python ML model.
+ *  The model then returns a list of predictions as JSON at given date as a response.
+ *  These JSON responses are just printed to the web page without formatting for now.
+ * 
+ *  Date string is currently sent as a query parameter.
+ *  If query param is not sent, defaults to todays date.
+ * 
+ * AUTHOR
+ *  Brandan Quinn
+ * 
+ * DATE
+ *  2/6/19 5:42pm 
+ */
+app.post('/api/predict-by-date', (req, res) => {
+    // let date = req.headers.referer.slice(-8);
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString(); // convert Buffer to string
+    });
+    req.on('end', () => {
+        console.log(body);
+        // res.end('ok');
+
+        let date = get(JSON.parse(body), 'date');
+
+        // console.log('DATE IS: ', date);
+        // request
+        // .post('http://127.0.0.1:5000/predict', { json: { date }})
+        // .on('data', (chunk) => {
+        //     console.log(chunk.toString());
+        //     const predictions_json = chunk.toString();
+        //     const converted_predictions = get(JSON.parse(predictions_json), 'predictions').map(pred => convert_prediction(pred));
+        //     console.log(converted_predictions);
+        //     res.send(converted_predictions);
+        // })
+
+        const converted_predictions = get(mock_pred, 'predictions').map(pred => convert_prediction(pred));
+        res.send(converted_predictions);
+    })
+    
+    
+    // If date not passed as a query param, get todays games.
+    
+})
+
+/**
+ * '/matchup-predict'
+ * 
+ * NAME
+ *  '/matchup-predict'
+ * 
+ * DESCRIPTION
+ *  Sets up '/matchup-predict' route for web app to send a POST request with two team IDs to python ML model.
+ *  The model then returns a prediction of a matchup between those two teams as a response.
+ *  These JSON responses are just printed to the web page without formatting for now.
+ * 
+ *  Date string is currently sent as a query parameter.
+ *  If query param is not sent, defaults to todays date.
+ * 
+ * AUTHOR
+ *  Brandan Quinn
+ * 
+ * DATE
+ *  5/1/19 4:34pm 
+ */
+app.post('/api/matchup-predict', (req, res) => {
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        let t1 = get(JSON.parse(body), 't1');
+        let t2 = get(JSON.parse(body), 't2');
+
+        // request
+        // .post('http://127.0.0.1:5000/matchup', { json: { t1, t2 }})
+        // .on('data', (chunk) => {
+        //     const predictions_json = chunk.toString();
+        //     const converted_predictions = get(JSON.parse(predictions_json), 'predictions').map(pred => convert_prediction(pred));
+        //     console.log(converted_predictions);
+        //     res.send(converted_predictions);
+        // })
+        const converted_predictions = get(mock_pred, 'predictions').map(pred => convert_prediction(pred));
+        res.send(converted_predictions);
+    });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ backend/utils/convert-predictions.js
@@ -0,0 +1,64 @@
+const get = require('lodash/get');
+
+
+/**
+ * NAME: convert_prediction()
+ * 
+ * PURPOSE: Converts raw prediction JSON from Flask API and converts it into user-friendly info for React app.
+ * 
+ * PARAMS: 
+ *  - game_pred, JSON object for each prediction
+ *      - contains keys: t1 (team 1), t2 (team 2), predicted-outcome (W/L), and predicted-pointdiff(# val)
+ * 
+ * SUMMARY:
+ *  If T1 predicted to Win against T2:
+ *      Prediction Message will be: T1 will win by (Rounded point differential)
+ *  Else:
+ *      Prediction Message will be: T2 will win by (Absolute value of rounded point differential) 
+ *          (absolute value needed as negative value indicates number of points T1 will lose by)
+ * 
+ * RETURNS:
+ *  JSON containing Teams playing and predicted result message of the game.
+ * 
+ * AUTHOR:
+ *  - Brandan Quinn
+ * 
+ * DATE:
+ *  12:03pm 4/3/19
+ */
+exports.convert_prediction = (game_pred) => {
+    // Pull off T1 and T2 
+
+    // If predicted-outcome is W, print T1 will win by (predicted-pointdiff)
+    // Else: print T2 will win by ((-1)*predicted-pointdiff)
+
+    let prediction_message;
+    let actual_result;
+
+    // If point differential rounds to 0, NBA games cannot tie - therefore I've decided to alert the user that the game was too close for 
+    // either outcome to be probable.
+    if (Math.round(get(game_pred, 'predictedPointdiff')) == 0) {
+        prediction_message = 'Too close to call.';
+    } else if (get(game_pred, 'predictedOutcome') == 'W') {
+        prediction_message = get(game_pred, 't1') + ' will win by: ' + Math.round(get(game_pred, 'predictedPointdiff')) + 'pts';
+    } else {
+        prediction_message = get(game_pred, 't2') + ' will win by: ' + (-1 * Math.round(get(game_pred, 'predictedPointdiff'))) + 'pts';
+    }
+
+    console.log("point diff: ", get(game_pred, 'actual-point-diff'));
+    if (get(game_pred, 'actual-point-diff')) {
+        if (get(game_pred, 'actual-point-diff') > 0) {
+            actual_result = get(game_pred, 't1') + ' won by: ' + (get(game_pred, 'actual-point-diff')) + 'pts';   
+        } else {
+            actual_result = get(game_pred, 't2') + ' won by: ' + (-1 * (get(game_pred, 'actual-point-diff'))) + 'pts';
+        }
+    }
+
+
+    return {
+        playing: get(game_pred, 't1') + ' vs. ' + get(game_pred, 't2'),
+        prediction_message,
+        actual_result,
+        is_outcome_correct: get(game_pred, 'is-outcome-correct')
+    }
+}
